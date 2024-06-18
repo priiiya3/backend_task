c41f7b3603bb1e5a3239e01f8370bd6fa88dbe6c
priiiya3 <priyasingh3709@gmail.com>
2024-06-18 23:00:02 +0530

    moved index.html

--- server.js
+++ server.js
@@ -5,31 +5,21 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const questionsRoutes = require('./routes/questions');
 const usersRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
-const { authenticate } = require('./controllers/authController');
-
-// serce static files
-app.use(express.static('public'));
-
-app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/questions', questionsRoutes);
 app.use('/api/users', usersRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-
-// const { sequelize } = require('./models');
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
